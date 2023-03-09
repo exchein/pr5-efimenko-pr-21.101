4f63e9d8a80bebaf47e8eca153f12e89459d368f
exchein <92368517+exchein@users.noreply.github.com>
2023-03-09 15:56:04 +0700

    Add files via upload

---
+++ efimenko33/app/src/main/java/com/example/efimenko33/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.efimenko33;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
